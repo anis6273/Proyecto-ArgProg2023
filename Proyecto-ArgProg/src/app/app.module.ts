650284d0a0a3f56326924c88ec07a93e186e3581
Ana Carla Lopez <anis6273@gmail.com>
2023-04-03 22:10:00 -0300

    carga bootstrap

--- Proyecto-ArgProg/src/app/app.module.ts
+++ Proyecto-ArgProg/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
 import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
