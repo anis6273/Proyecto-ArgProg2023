e5bd2318dfc803b5e342b6ef8227fe1dc5f958d8
Ana Carla Lopez <anis6273@gmail.com>
2023-04-16 01:17:13 -0300

    movido a BackEnd

---
+++ Proyecto-ArgProg/BackEnd/src/main/java/com/portfolio/aclf/AclfApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.aclf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclfApplication.class, args);
+	}
+
+}
---
+++ Proyecto-ArgProg/BackEnd/src/test/java/com/portfolio/aclf/AclfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.aclf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
